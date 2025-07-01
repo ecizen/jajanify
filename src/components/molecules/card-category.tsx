2ddcaddd5e6bbdecb5b58d0c6d940017a7110306
hardiektatendra <hardiektatendta@gmail.com>
2025-07-01 08:06:36 +0700

    update

--- src/components/molecules/card-category.tsx
+++ src/components/molecules/card-category.tsx
@@ -6,6 +6,7 @@ import { Heart } from 'lucide-react';
 import type { StaticImageData } from "next/image";
 
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 type filteredData = {
     id: number;
@@ -22,8 +23,11 @@ type TravelData ={
 
 
 const CardCategory: React.FC<TravelData> = ({travel}) => {
+  const route =useRouter()
 
-  
+  const handle =() => {
+    route.push('/detail-travel')
+  }
   return (
     <Swiper
       spaceBetween={20}
@@ -46,7 +50,7 @@ const CardCategory: React.FC<TravelData> = ({travel}) => {
       {travel && travel.map((item: filteredData) => (
 
           <SwiperSlide key={item.id}>
-          <div className="bg-white rounded-lg shadow  relative flex justify-center  w-full h-[260px]">
+          <div onClick={handle} className="bg-white rounded-lg shadow  relative flex justify-center  w-full h-[260px]">
             <div className='absolute max-w-[90%] rounded-xl w-full bg-white/30 backdrop-blur-sm  bottom-3 p-2 px-4 flex items-center justify-between'>
                 <div>
                     <h3 className="mt-2 text-sm font-semibold line-clamp-1 text-white">{item.name}</h3>
